04f73074d68b76341da6f3547d2050d6756a2268
maqsooddbaloch <fa16bscs0012@gmail.com>
2020-01-26 23:59:36 +0500

    fs

---
+++ app/src/main/java/com/example/signin/Onboarding.java
@@ -0,0 +1,57 @@
+package com.example.signin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.hololo.tutorial.library.Step;
+import com.hololo.tutorial.library.TutorialActivity;
+
+public class Onboarding extends TutorialActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addFragment(new Step.Builder().setTitle("This is header")
+                .setContent("This is content")
+                .setBackgroundColor(Color.parseColor("#FF0957")) // int background color
+                .setDrawable(R.drawable.ic_launcher_background) // int top drawable
+                .setSummary("This is summary")
+                .build());
+
+        addFragment(new Step.Builder().setTitle("This is header")
+                .setContent("This is content")
+                .setBackgroundColor(Color.parseColor("#FF0957")) // int background color
+                .setDrawable(R.drawable.ic_launcher_foreground) // int top drawable
+                .setSummary("This is summary")
+                .build());
+
+
+        addFragment(new Step.Builder().setTitle("This is header")
+                .setContent("This is content")
+                .setBackgroundColor(Color.parseColor("#FF0957")) // int background color
+                .setDrawable(R.drawable.ic_launcher_background) // int top drawable
+                .setSummary("This is summary")
+                .build());
+
+
+    }
+
+    @Override
+    public void finishTutorial() {
+        Intent i = new Intent(Onboarding.this,MainActivity.class);
+        startActivity(i);
+    }
+
+    @Override
+    public void currentFragmentPosition(int position) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
---
+++ app/src/main/java/com/example/signin/Splashscreen.java
@@ -0,0 +1,32 @@
+package com.example.signin;
+
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+public class Splashscreen extends AppCompatActivity {
+
+    ProgressBar splashProgress;
+    int SPLASH_TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do any action here. Now we are moving to next page
+                Intent mySuperIntent = new Intent(Splashscreen.this, Main2Activity.class);
+                startActivity(mySuperIntent);
+
+                //This 'finish()' is for exiting the app when back button pressed from Home page which is ActivityHome
+                finish();
+
+            }
+        }, SPLASH_TIME);
+    }
+}
+
